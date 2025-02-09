ae2cae639ea332a643dbbbc3fe4a0392396712a9
ter <136942646+tylerglare@users.noreply.github.com>
2025-02-09 22:51:30 +0800

    Update config.py

--- pygame/config.py
+++ pygame/config.py
@@ -1,9 +1,16 @@
-WIN_WIDTH = 640
-WIN_HEIGHT = 320
+WIN_WIDTH = 800
+WIN_HEIGHT = 512
 TILESIZE = 32
 FPS = 60
 
-PLAYER_LAYER = 3
+PLAYER_LAYER = 4
+ENEMY_LAYER = 3
+BLOCK_LAYER = 2
+GROUND_LAYER = 1
+TILESIZE = 32
+FPS = 60
+
+PLAYER_LAYER = 4
 ENEMY_LAYER = 3
 BLOCK_LAYER = 2
 GROUND_LAYER = 1
@@ -17,19 +24,20 @@ BLUE = (0, 0, 255)
 WHITE = (255, 255, 255)
 
 tilemap = [
-  'BBBBBBBBBBBBBBBBBBBB',
-  'B..................B',
-  'B..................B',
-  'B...BBB............B',
-  'B..................B',
-  'B.........P....E...B',
-  'B..................B',
-  'B....BBB...........B',
-  'B......B...........B',
-  'B......B...........B',
-  'B..................B',
-  'B............E.....B',
-  'B..................B',
-  'B..................B',
-  'BBBBBBBBBBBBBBBBBBBB',
+  'BBBBBBBBBBBBBBBBBBBBBBBBB',
+  'B..E...............B....B',
+  'B.....BBBB......B.......B',
+  'B...BBB.................B',
+  'B.......................B',
+  'B.........P....E........B',
+  'B................B......B',
+  'B....BBBBB..............B',
+  'B......B................B',
+  'B.......................B',
+  'B........BBBB...........B',
+  'B...........BE..........B',
+  'B..BB.......BBBB........B',
+  'B......E..........E.....B',
+  'B.......................B',
+  'BBBBBBBBBBBBBBBBBBBBBBBBB',
 ]
